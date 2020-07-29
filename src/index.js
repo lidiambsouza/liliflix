a6875ecd8081e4edbba52e5a747e266ded85761e
lidiambsouza <lidiambsouza@gmail.com>
2020-07-28 22:26:33 -0300

    rotas, pagina default e telas cadastro

--- src/index.js
+++ src/index.js
@@ -1,13 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './reset.css';
+import Home from './pages/Home/App';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
 
+import {
+ BrowserRouter,
+ Switch,
+ Route
+} from 'react-router-dom';
+
+/*function CadastroVideo(){
+  return(
+    <div>
+      Página de Cadastro de vídeo
+    </div>
+  )
+}*/
+
+const Pagina404 = () => (<div>Página 404</div>)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  //<React.StrictMode>
+    //<App />
+  //</React.StrictMode>,
+  <BrowserRouter>
+   <Switch>
+     <Route path="/" component ={Home} exact />
+     <Route path="/cadastro/video" component={CadastroVideo} exact/>
+     <Route path="/cadastro/categoria" component={CadastroCategoria} exact/>
+     <Route component={Pagina404}/>
+   </Switch>
+ 
+  </BrowserRouter>,
+
   document.getElementById('root')
 );
 
