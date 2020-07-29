1e0f25b8f7f93a4d5c8bbd7f9e007dab4bd4b7d0
lidiambsouza <lidiambsouza@gmail.com>
2020-07-28 23:16:48 -0300

    inicio do form de categoria

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
+import Button from '../../../components/Button';
 
 function CadastroCategoria(){
     return(
       <PageDefault>
         <h1>Cadastro de Categoria</h1>
+        <form>
+            <label>Nome da Categoria:
+                <input type="text"/>
+            </label>
+            <Button>
+                Cadastrar
+            </Button>
+        </form>
 
-        
+
         <Link to="/">
             Ir para home
         </Link>
